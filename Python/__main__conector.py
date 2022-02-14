a6f86c0ffe21a154e5b25b4b94995d74a2813fc7
Izabela Schich <izabela.schick@gmail.com>
2022-02-14 11:38:02 -0300

    .

--- Python/__main__conector.py
+++
@@ -1,5 +0,0 @@
-from modules.connector import Interface_mysql
-
-banco = Interface_mysql("admin", "admin", "localhost", "teste")
-dados = banco.selecionar("SELECT * FROM funcionarios")
-print(dados)
